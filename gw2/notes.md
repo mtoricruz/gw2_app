147cca6290e9cde15017c4634576ca85af974f9b
toricruzmendiola <mtoricruz@gmail.com>
2020-07-11 12:55:00 -0700

    file scaffolding is complete and added notes for future reference and idea adding

---
+++ gw2/notes.md
@@ -0,0 +1,14 @@
+Tasks:
+1.  [] Display Profile Card:
+    a. [] Name/Wallet/Bank/Mat Storage/Created Date
+    b. [] Class/Mounts/Achievements(+Points)/Location?
+
+2.  [] Create Basic Routing Paths:
+    a. [] My APIKey as placeholder/default
+    b. [] Make APIKey dynamic in url to allow other's to paste theirs in
+
+3.  [] Add basic styling to add depth and separation between data
+
+4.  [] Link to my portfolio site/LinkedIn/Github
+
+5.  [] 
--- gw2/src/actions/profileAction.js
+++ gw2/src/actions/profileAction.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 export const FETCH_PROFILE_START = 'FETCH_PROFILE_START';
 export const FETCH_PROFILE_SUCCESS = 'FETCH_PROFILE_SUCCESS';
 export const FETCH_PROFILE_FAILURE = 'FETCH_PROFILE_FAILURE';
-export const fetchProfile = () => {
+export const fetchProfile = (apikey) => {
     return dispatch => {
         dispatch({ type: FETCH_PROFILE_START})
         axios
-            .get(`https://api.guildwars2.com/v2`)
+            .get(`https://api.guildwars2.com/v2/account/${apikey}`)
             .then(res => {
                 console.log('API CONSOLE LOG', res.data)
                 dispatch({ type: FETCH_PROFILE_SUCCESS, payload: res})
