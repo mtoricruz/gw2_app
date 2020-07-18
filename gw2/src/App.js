c6a14f1fac80b86fd6f252d94c6420b496cebd6f
toricruzmendiola <mtoricruz@gmail.com>
2020-07-18 14:31:57 -0700

    adding api/utils to properly access gw2 api key. Read documentation that mentioned i can access authentication via query parameter so iam referencing another gw2 player's project to give me an idea. From their idea i will transfer functions into redux store.

--- gw2/src/App.js
+++ gw2/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import Profile from './components/Profile'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Profile from './components/Profile';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Profile Stuff</p>
-        <Profile />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <p> Navigation placeholder</p>
+        </nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Profile Stuff</p>
+        </header>
+        <Route path='/:id' component={Profile}/>
+      </div>
+    </Router>
   );
 }
 
